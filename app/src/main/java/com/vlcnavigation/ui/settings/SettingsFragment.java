e0effae9893b89ebd1af4fcf36f1bbc0754ef9ef
EspylArva <tchong.kite.h@gmail.com>
2021-03-05 22:24:51 +0100

    Few GUI fixes. Added button to add a light object. More fixes required to avoid errors.

--- app/src/main/java/com/vlcnavigation/ui/settings/SettingsFragment.java
+++ app/src/main/java/com/vlcnavigation/ui/settings/SettingsFragment.java
@@ -5,6 +5,7 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
@@ -14,6 +15,8 @@ import androidx.lifecycle.ViewModelProvider;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.android.material.textfield.TextInputEditText;
 import com.vlcnavigation.R;
 import com.vlcnavigation.components.RecyclerViewMargin;
 import com.vlcnavigation.module.trilateration.Light;
@@ -28,6 +31,10 @@ public class SettingsFragment extends Fragment {
     private SettingsViewModel settingsViewModel;
     private RecyclerView recycler_lights;
     private TextView textView;
+    private FloatingActionButton fab_addLight;
+    private TextInputEditText txt_newLightXPos, txt_newLightYPos, txt_newLightDescription;
+
+    private LightAdapter lightAdapter;
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
@@ -37,13 +44,19 @@ public class SettingsFragment extends Fragment {
         initObservers();
         initListeners();
 
+
+        lightAdapter = new LightAdapter(settingsViewModel.getListOfLights().getValue());
+//        lightAdapter = new LightAdapter(new ArrayList<Light>(Arrays.asList(l1, l2, l3)));
+        recycler_lights.setAdapter(lightAdapter);
+
         Light l1 = new Light.Builder(3, 2).setDescription("Light in the corridor #1").setDistance(20).build();
-        Light l2 = new Light.Builder(1, 2).setDescription("Light in Prof. Zhang's room").setDistance(24).build();
+        Light l2 = new Light.Builder(1, 2).setDescription("Light in Prof. Zhang's office").setDistance(24).build();
         Light l3 = new Light.Builder(5, 3).setDescription("Light in the corridor #5").setDistance(40).build();
-        Timber.d("Lights:" + '\n' + l1.toString() + '\n' + l2.toString() + '\n' + l3.toString());
 
-        recycler_lights.setAdapter(new LightAdapter(new ArrayList<Light>(Arrays.asList(l1, l2, l3))));
-
+        // Template data
+        settingsViewModel.addLight(l1);
+        settingsViewModel.addLight(l2);
+        settingsViewModel.addLight(l3);
 
         return root;
     }
@@ -58,6 +71,12 @@ public class SettingsFragment extends Fragment {
         LinearLayoutManager recycler_layout = new LinearLayoutManager(getContext());
         recycler_layout.setOrientation(LinearLayoutManager.VERTICAL);
         recycler_lights.setLayoutManager(recycler_layout);
+
+        fab_addLight = root.findViewById(R.id.btn_addLight);
+
+        txt_newLightDescription = root.findViewById(R.id.txt_addLight_description);
+        txt_newLightXPos = root.findViewById(R.id.txt_addLight_xPos);
+        txt_newLightYPos = root.findViewById(R.id.txt_addLight_yPos);
 
         textView = root.findViewById(R.id.text_notifications);
         return root;
@@ -75,6 +94,23 @@ public class SettingsFragment extends Fragment {
 
     private void initListeners()
     {
+        fab_addLight.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
 
+                // Add a new light
+                Light newLight = new Light.Builder(Double.parseDouble(txt_newLightXPos.getText().toString()), Double.parseDouble(txt_newLightYPos.getText().toString()))
+                        .setDescription(txt_newLightDescription.getText().toString()).build();
+                settingsViewModel.addLight(newLight);
+
+                // Reset UI (Add button part)
+                txt_newLightDescription.getText().clear();
+                txt_newLightXPos.getText().clear();
+                txt_newLightYPos.getText().clear();
+
+                // UX
+                Toast.makeText(getContext(), String.format("Successfully added a new light!"), Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 }
--- app/src/main/java/com/vlcnavigation/ui/settings/SettingsViewModel.java
+++ app/src/main/java/com/vlcnavigation/ui/settings/SettingsViewModel.java
@@ -4,16 +4,29 @@ import androidx.lifecycle.LiveData;
 import androidx.lifecycle.MutableLiveData;
 import androidx.lifecycle.ViewModel;
 
+import com.vlcnavigation.module.trilateration.Light;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class SettingsViewModel extends ViewModel {
 
     private MutableLiveData<String> mText;
+    private MutableLiveData<List<Light>> mListOfLights;
 
     public SettingsViewModel() {
         mText = new MutableLiveData<>();
+        mListOfLights = new MutableLiveData<>();
+
+
         mText.setValue("This is notifications fragment");
+        mListOfLights.setValue(new ArrayList<Light>());
     }
 
     public LiveData<String> getText() {
         return mText;
     }
+
+    public void addLight(Light newLight) { mListOfLights.getValue().add(newLight); }
+    public LiveData<List<Light>> getListOfLights() { return mListOfLights; }
 }
