8a6c7b03fe389d8888821e4773ba061e7d2b0139
Tristan <tristan.ghodsy@isep.fr>
2021-06-02 21:53:36 +0200

    Wav file FFT + Microphone + GUI params

--- app/src/main/java/com/vlcnavigation/ui/fft/FFTFragment.java
+++ app/src/main/java/com/vlcnavigation/ui/fft/FFTFragment.java
@@ -5,11 +5,16 @@ import android.media.AudioRecord;
 import android.media.MediaRecorder;
 import android.os.Bundle;
 import android.os.Environment;
+import android.provider.VoicemailContract;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.SeekBar;
 import android.widget.TextView;
+import android.widget.Toast;
 import android.widget.ToggleButton;
 
 import androidx.annotation.NonNull;
@@ -25,11 +30,14 @@ import com.vlcnavigation.MainActivity;
 import com.vlcnavigation.R;
 import com.vlcnavigation.module.audiorecord.SignalView;
 
+import org.apache.commons.lang3.ArrayUtils;
 import org.jtransforms.fft.DoubleFFT_1D;
 
 import java.io.File;
 import java.io.FileInputStream;
 import java.util.Arrays;
+
+import timber.log.Timber;
 
 
 public class FFTFragment extends Fragment {
@@ -37,9 +45,11 @@ public class FFTFragment extends Fragment {
     private FFTViewModel FFTViewModel;
     private SignalView signalView;
     private ToggleButton tB;
+    private Button btnAnalyse;
+    private EditText edtName,edtOffset,edtSampleRate;
     protected SeekBar sensBar;
     protected SeekBar sensBarSmooth;
-    protected TextView tvFreq;
+    protected TextView tvLiveFreq, tvWavFreq;
 
 
     //plot
@@ -54,8 +64,14 @@ public class FFTFragment extends Fragment {
     protected double fftData[]= new double[1024];
     protected int[] oldSentData=new int[3];
     protected SimpleXYSeries series1;
-    protected double offset = 21.428;
-    protected double frequency = 0;
+    protected double liveFrequency = 0;
+    protected double wavFrequency = 0;
+
+    //parameters
+    protected double liveOffset = 21.428;
+    protected double wavOffset = 0.07557265176877;
+    double sampleRate = 44100;
+
 
     //audio record
     private AudioRecord audioInput = null;
@@ -82,6 +98,8 @@ public class FFTFragment extends Fragment {
         //initialize the fft_plot
         initPlot();
 
+
+
         //start the thread to read microphone live audio fft and then plot
         //the trigger is the toggle button
         liveAudioFFT();
@@ -89,9 +107,44 @@ public class FFTFragment extends Fragment {
 
 
 
-
         //start the wav file audio FFT (results in an array, printed in the terminal)
-        //wavAudioFFT();
+        btnAnalyse.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                String sampleRate = edtSampleRate.getText().toString();
+
+                if (sampleRate.matches("")) {
+                    Toast.makeText(getActivity(), "You did not enter a sample rate. 44100Hz by default.", Toast.LENGTH_SHORT).show();
+                    Timber.d(TAG,FFTFragment.this.sampleRate);
+                    FFTFragment.this.sampleRate = 44100;
+                }
+                else {
+                    FFTFragment.this.sampleRate = Double.parseDouble(edtSampleRate.getText().toString());
+                }
+
+                String offset = edtOffset.getText().toString();
+
+                if (offset.matches("")) {
+                    Toast.makeText(getActivity(), "You did not enter an offset. 21.428 by default.", Toast.LENGTH_SHORT).show();
+                    FFTFragment.this.liveOffset = 21.428;
+                    Timber.d(TAG,FFTFragment.this.liveOffset);
+
+                }
+                else {
+                    FFTFragment.this.liveOffset = Double.parseDouble(edtOffset.getText().toString());
+                }
+
+                wavAudioFFT();
+            }
+        });
+
+
+
+
+
+
+
 
 
 
@@ -128,9 +181,17 @@ public class FFTFragment extends Fragment {
         tB = root.findViewById(R.id.toggleButton2);
         sensBar = root.findViewById(R.id.seekBar);
         sensBarSmooth = root.findViewById(R.id.seekBarSmooth);
-        tvFreq = root.findViewById(R.id.tvFreq);
+        tvLiveFreq = root.findViewById(R.id.tv_livefreq);
+        tvWavFreq = root.findViewById(R.id.tv_wavfreq);
+        btnAnalyse = root.findViewById(R.id.btn_analyse);
 
-        signalView.sndAudioBuf(MainActivity.BUFFER, MainActivity.BUFFER_READ_RESULT);
+        btnAnalyse = root.findViewById(R.id.btn_analyse);
+        edtName = root.findViewById(R.id.edt_name);
+        edtOffset= root.findViewById(R.id.edt_liveoffset);
+        edtSampleRate= root.findViewById(R.id.edt_samplerate);
+
+
+        //signalView.sndAudioBuf(MainActivity.BUFFER, MainActivity.BUFFER_READ_RESULT);
 
 
 
@@ -164,6 +225,7 @@ public class FFTFragment extends Fragment {
 
         // add a new series' to the xyplot:
         plot_fft.addSeries(series1, series1Format);
+
 
         // reduce the number of range labels
 
@@ -202,21 +264,18 @@ public class FFTFragment extends Fragment {
         isRecording = true;
 
 
+
+
         recordingThread = new Thread(new Runnable() {
+
+
 
 
 
 
             public void run() {
 
-                //update textview
-                getActivity().runOnUiThread(new Runnable() {
-                    @Override
-                    public void run() {
-                        // This code will always run on the UI thread, therefore is safe to modify UI elements.
-                        tvFreq.setText(String.valueOf(frequency));
-                    }
-                });
+
 
 
                 while (isRecording) {
@@ -243,8 +302,8 @@ public class FFTFragment extends Fragment {
                         //System.out.println("fftData = "+Arrays.toString(fftData));
 
                         //Frequency
-                        frequency = calculateFrequency(fftData);
-                        System.out.println("Frequency : "+ frequency);
+                        liveFrequency = calculateFrequency(fftData, liveOffset,true);
+                        System.out.println("Frequency : "+ liveFrequency);
 
 
 
@@ -256,8 +315,9 @@ public class FFTFragment extends Fragment {
                             series1.addLast(null, fftData[j * powOf2temp] * powOf2temp);
                         }
                         int sensBarProgress = sensBar.getProgress();
-                        plot_fft.setRangeBoundaries(0, (100-((sensBarProgress==100)?99:sensBarProgress)) * 1000, BoundaryMode.FIXED);
+                        //plot_fft.setRangeBoundaries(0, (100-((sensBarProgress==100)?99:sensBarProgress)) * 1000, BoundaryMode.FIXED);
                         plot_fft.redraw();
+                        Log.d(TAG,"plot redrawn");
                         ////////////////
 
                         final int dataToSend[] = new int[3];
@@ -293,6 +353,9 @@ public class FFTFragment extends Fragment {
                     }
                 }
             }
+
+
+
         }, "AudioRecorder Thread");
         recordingThread.start();
         ////////////////////////////////////////////////////////////////////////////////////////////
@@ -300,8 +363,19 @@ public class FFTFragment extends Fragment {
     }
 
     private void wavAudioFFT(){
-        File file = null;file = new File(Environment.getExternalStorageDirectory()+"/"+"Download/sin_1000.wav");
+
+        String filename = edtName.getText().toString();
+
+        if (filename.matches("")) {
+            Toast.makeText(getActivity(), "You did not enter a filename", Toast.LENGTH_SHORT).show();
+            Timber.d("No filename entered");
+            return;
+        }
+
+
+        File file = null;file = new File(Environment.getExternalStorageDirectory()+"/"+"Download/"+filename+".wav");
         byte[] byteData = new byte[(int) file.length()];
+        System.out.println("Using "+file.getName()+" ___________________________________________________");
         FileInputStream in = null;
         try {
             in = new FileInputStream( file );
@@ -310,26 +384,75 @@ public class FFTFragment extends Fragment {
         catch (Throwable throwable){
             throwable.printStackTrace();
         }
+        System.out.println("Wav audio file byteData"+Arrays.toString(byteData));
 
 
+        //creation of the audio array
+        double[] fftDataWav = new double[byteData.length];
+        double[] absFftDataWav = new double[byteData.length];
 
+
+        // Convert and put sData short array into fftDataWav double array to perform FFT
+        for (int j = 0; j < byteData.length; j++) {
+            fftDataWav[j] = (double) byteData[j];
+        }
+
+        System.out.println("Wav audio file fftDataWav double converted"+Arrays.toString(fftDataWav));
+
+
+        // Fast Fourier Transform from JTransforms
+        final DoubleFFT_1D fftWav = new DoubleFFT_1D(fftDataWav.length);
+        // Perform 1D fft
+        fftWav.realForward(fftDataWav);
+        System.out.println("Wav audio file fftDataWav after FFT = "+Arrays.toString(fftDataWav));
+
+        //convert abs values
+        for (int j = 0; j < fftDataWav.length; j++){
+            double x = fftDataWav[j];
+            //System.out.println("fftDataWav[j] = "+x+" | ");
+            //System.out.println("Math.abs(fftDataWav[j]) = "+Math.abs(x)+" | ");
+            absFftDataWav[j] = Math.abs(x);
+        }
+        System.out.println("absFftDataWav = "+Arrays.toString(absFftDataWav));
+
+        wavFrequency = calculateFrequency(absFftDataWav,wavOffset,false);
+
+        tvWavFreq.setText(String.valueOf(wavFrequency));
+
+        System.out.println("Wav Audio File Frequency : "+ wavFrequency);
+
+
+        /* OLD method
         absNormalizedSignal = calculateFFT(byteData);
 
-        System.out.println("Data FFT absNormalizedSignal : "+Arrays.toString(absNormalizedSignal));
-        System.out.println("Data FFT peak : "+mPeakPos);
+
+
+        //System.out.println("Data FFT absNormalizedSignal : "+Arrays.toString(absNormalizedSignal));
+        //System.out.println("Data FFT peak : "+mPeakPos);
+        */
+
     }
 
-    public double calculateFrequency(double[] fft)
+    public double calculateFrequency(double[] fft,double offset,boolean live)
     {
         double[] fftSorted = fft.clone();
         Arrays.sort(fftSorted);
+        ArrayUtils.reverse(fftSorted);
+        System.out.println("fftSorted = "+Arrays.toString(fftSorted));
+        ArrayUtils.reverse(fftSorted);
         double peak1 = fftSorted[fftSorted.length-1];
         //System.out.println("Peak 1 = "+peak1);
         double peak2 = fftSorted[fftSorted.length-2];
         //System.out.println("Peak 2 = "+peak2);
+        double peak3 = fftSorted[fftSorted.length-3];
+        //System.out.println("Peak 2 = "+peak2);
+        double peak4 = fftSorted[fftSorted.length-4];
+        //System.out.println("Peak 2 = "+peak2);
+        double peak5 = fftSorted[fftSorted.length-5];
+        //System.out.println("Peak 2 = "+peak2);
 
 
-        int index1 = 0; int index2 = 0;
+        int index1 = 0; int index2 = 0; int index3 = 0;int index4 = 0;int index5 = 0;
         for(int i=0; i<fft.length; i++) {
             if(fft[i] == peak1) {
                 index1 = i;
@@ -337,11 +460,52 @@ public class FFTFragment extends Fragment {
             else if(fft[i] == peak2) {
                 index2 = i;
             }
+            else if(fft[i] == peak3) {
+                index3 = i;
+            }
+            else if(fft[i] == peak4) {
+                index4 = i;
+            }
+            else if(fft[i] == peak5) {
+                index5 = i;
+            }
         }
-        //System.out.println("Index 1 = "+index1);
-        //System.out.println("Index 2 = "+index2);
+        System.out.println("Index 1 = "+index1+"/"+fft.length);
+        System.out.println("Index 2 = "+index2+"/"+fft.length);
+        System.out.println("Index 3 = "+index3+"/"+fft.length);
+        System.out.println("Index 4 = "+index4+"/"+fft.length);
+        System.out.println("Index 5 = "+index5+"/"+fft.length);
 
-        return ((index1+index2)/2)*offset;
+        if (live){
+            if (offset != 0) {
+                return ((index1+index2)/2)*offset;
+            }
+            else{
+                return ((index1+index2)/2)*1;
+            }
+        }
+        else {
+
+
+
+
+
+
+
+            System.out.println("1st frequency = "+((index1) * this.sampleRate) / fft.length);
+            System.out.println("2nd frequency = "+((index2) * this.sampleRate) / fft.length);
+            System.out.println("3rd frequency = "+((index3) * this.sampleRate) / fft.length);
+            System.out.println("4th frequency = "+((index4) * this.sampleRate) / fft.length);
+            System.out.println("5th frequency = "+((index5) * this.sampleRate) / fft.length);
+
+
+            return ((index1) * this.sampleRate) / fft.length ;
+
+        }
+
+
+
+
     }
 
 
